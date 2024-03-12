825346d770ec6af0a2c58ea5a2be335ee05b805f
Flavio <flavioescalegari@gmail.com>
2024-03-12 11:20:39 -0600

    Finalizing the front-end part

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/*Price slider*/
 document.getElementById("maxPrice").addEventListener("input", function() {
     const priceOutput = document.getElementById("priceOutput");
     const formattedPrice = new Intl.NumberFormat('en-CA', { style: 'currency', currency: 'CAD' }).format(this.value);
@@ -7,3 +8,14 @@ document.getElementById("maxPrice").addEventListener("input", function() {
   document.getElementById("btn-search").addEventListener("click", function() {
     console.log("OK");
   });
+
+/*Rotate img with randon degree*/
+const imageElement = document.getElementById('image-grid'); 
+
+function applyRandomRotation() {
+  const randomDeg = Math.random() * 30 - 15; // Range: -15 to 15
+  imageElement.style.transform = `rotate(${randomDeg}deg)`; 
+}
+
+// Call the function on hover, page load, on an interval, etc.
+imageElement.addEventListener('mouseover', applyRandomRotation);
